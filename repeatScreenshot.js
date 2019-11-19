286393503758f9362fbec0559ee3a04078f092f9
Daniel Jones <messyjones@gmail.com>
2019-11-18 20:55:10 -0500

    moved the divide photo to the repeat class

--- repeatScreenshot.js
+++ repeatScreenshot.js
@@ -9,11 +9,36 @@ function takeScreenshot() {
     var Screenshot = require('./takeScreenshot');
     Screenshot.takeScreenshot().then(function() {
         console.log("took new screenshot");
-        var OCR = require('./ocr/readScreenshot');
-        OCR.runReader("forest_quad");
-    }, function(error) {
-        console.log(error);
-    });
+        dividePhoto().then(function() {
+            var OCR = require('./ocr/readScreenshot');
+            OCR.runReader("forest_quad");
+        })
+    })
+}
+
+function dividePhoto() {
+    var imageToSlices = require('image-to-slices');
+    var width = 3360 / 3;
+    var nextWidth = 3360 * (2/3);
+    var firstX = 1100;
+    var secondX = 2100 * (2/3);
+    var lineXArray = [firstX, secondX];
+    var lineYArray = [width, nextWidth];
+    var source = './screenshot.png';
+
+    var promise = new Promise(function(resolve, reject) {
+        imageToSlices(source, lineXArray, lineYArray, {
+            saveToDir: './ocr/photoSections',
+            clipperOptions: {
+                canvas: require('canvas')
+            }    
+        }, function() {
+            console.log("hiii");
+            resolve('the source image has been sliced into 9 sections!');
+        });
+    })
+
+    return promise;
 }
 
 
