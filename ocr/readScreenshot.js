788679bc7abd1df7a21a52431638aa81e15aa636
Daniel Jones <messyjones@gmail.com>
2019-11-18 20:33:55 -0500

    got the relativepaths working

--- ocr/readScreenshot.js
+++ ocr/readScreenshot.js
@@ -1,4 +1,4 @@
-process.env.GOOGLE_APPLICATION_CREDENTIALS = "/Users/danieljones/Documents/ocr-reader/ocr/ocr-reader-edcdf79258cd.json";
+process.env.GOOGLE_APPLICATION_CREDENTIALS = "ocr/ocr-reader-edcdf79258cd.json";
 
 const vision = require('@google-cloud/vision');
 
@@ -6,26 +6,44 @@ exports.runReader = async function runReader(box_identifier) {
     // Creates a client
     const client = new vision.ImageAnnotatorClient();
 
-    const fileName = '/Users/danieljones/Documents/ocr-reader/ocr/section-1.png';
+    // const dividePhoto = require('./dividePhoto');
+
+    const fileName = 'ocr/section-1.png';
 
     // Performs text detection on the local file
     const [result] = await client.textDetection(fileName);
     const detections = result.textAnnotations;
-    detections.forEach(text => console.log(text));
-    // getSurgeMultiplier(detections, box_identifier);
+
+    // detections.forEach(text => console.log(text));
+    getSurgeMultiplier(detections, box_identifier);
 }
 
 function getSurgeMultiplier(detections, box_identifier) {
     for (var i = 0; i < detections.length; i++) {
         var detection = detections[i];
         var description = detection.description;
-        if (description == "UberXL") {
-            var previousDetection = detections[i + 1];
+        var isMultiplierString = checkIfIsMultiplier(description);
+        if (isMultiplierString) {
             var multiplier = parseMultiplierString(previousDetection.description);
             if (!isNaN(multiplier)) {
                 saveRidesharePrice(box_identifier, multiplier);
             }
         }
+    }
+}
+
+//checking if last char is x
+//checking if first char is a number
+//That seems like it won't pull any false positives.
+function checkIfIsMultiplier(string) {
+    var lastChar = string.slice(-1);
+    if (lastChar == "x") {
+        var firstChar = string.charAt(0);
+        var number = Number(firstChar);
+        var isNumber = !isNaN(number);
+        return isNumber;
+    } else {
+        return false;
     }
 }
 
